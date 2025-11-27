60715c9a371f9f68620d832f0921c7ef4a439c2e
Rahma Magdy <00rahmamagdy@gmail.com>
2025-11-27 18:15:17 +0200

    hero

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-import Nav from "../Components/Nav"
+import Nav from "../Components/Nav";
+import rous from "../Assets/rous.gif"
+import './Home.css'
 
 const Home = () => {
     return (  <>
     <Nav/>
+
+    <section className="sec1">
+        <img src={rous} alt="hero" className="rous" />
+    </section>
+
     </> );
 }
  
