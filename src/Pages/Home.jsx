2f5f596c3b8c7f821c48b2017146479f5729da6f
Rahma Magdy <00rahmamagdy@gmail.com>
2025-12-02 19:55:47 +0200

    footer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,15 +6,18 @@ import Sec2 from '../components-home/Sec2';
 import Sec3 from '../components-home/Sec3';
 import Sec4 from '../components-home/Sec4';
 import Sec5 from '../components-home/Sec5';
+import Sec6 from '../components-home/Sec6';
 
 const Home = () => {
     return (  <>
+
     <Nav/>
     <Sec1/>
     <Sec2/>
     <Sec3/>
     <Sec4/>
     <Sec5/>
+    <Sec6/>
  
 
     </> );
---
+++ src/components-home/Sec6.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "../components-home/Sec6.css"
+import footer from "../Assets/footer.png"
+const Sec6 = () => {
+    return ( <>
+              
+              <section className='foot'>
+              <img src={footer} alt="end" className="footer" />
+              </section>
+       
+    </>  );
+}
+ 
+export default Sec6;
